43213a1088e9195d47137864ff5a7460e5a8b912
tienld235 <tienld@vietnamlab.vn>
2022-06-27 08:20:09 +0700

    implement demo

--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
     <div>
-      I'm some other page!
-      <Link to='/'>Go back home</Link>
+      Im some other page!
+      <Link to={"/"}>Go back home</Link>
     </div>
   );
 };
-
-
--- complex/server/index.js
+++ complex/server/index.js
@@ -1,78 +1,71 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
 // Express App Setup
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express(); //init sending and receiving http request
+app.use(cors()); // allow make request from one domain to different domain or different port
+app.use(bodyParser.json()); // parse body of request into json
 
-// Postgres Client Setup
-const { Pool } = require('pg');
+// PostgreSQL Client Setup
+const { Pool } = require("pg");
 const pgClient = new Pool({
   user: keys.pgUser,
-  host: keys.pgHost,
+  host: keys.pgUser,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: keys.pgPort
+  port: keys.pgPort,
 });
-pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
+});
 
 // Redis Client Setup
-const redis = require('redis');
+const redis = require("redis");
+
 const redisClient = redis.createClient({
   host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
+  port: keys.redisPost,
+  retry_strategy: () => 1000,
 });
+
+// duplicate because if we have a client that listening or publishing info on retests, when it does, it can not be used for other purposes.
 const redisPublisher = redisClient.duplicate();
 
-// Express route handlers
-
-app.get('/', (req, res) => {
-  res.send('Hi');
+// Express route handles
+app.get("/", (_, res) => {
+  res.send("Hi");
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
+app.get("/values/all", async (_, res) => {
+  const values = await pgClient.query("SELECT * FROM values");
   res.send(values.rows);
 });
 
-/* QUICK NOTE !!
- *
- * we doing a very classic callback without 'await'
- * cause redis library for NodeJS doesn't have out of box 'promise support'
- */
-
-app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
+app.get("/values/current", async (_, res) => {
+  redisClient.hgetall("values", (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.post("/values", async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
+    return res.status(422).send("Index too high");
   }
 
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("abcdxyz", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
   res.send({ working: true });
 });
 
-const port = process.env.PORT | 5002;
-
-app.listen(port, err => {
-  console.log(`NodeJS SERVER LISTENING on PORT: ${port}`)
+app.listen(5002, (err) => {
+  console.log("Ready! Listening");
 });
